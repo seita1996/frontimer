5e8f3882d3b85a61c716fa7f2643a4fb5ff958e6
seita1996 <taha.seita@gmail.com>
2022-11-22 10:51:39 +0900

    electron init

---
+++ README.md
@@ -0,0 +1,23 @@
+# frontimer
+
+Desktop timer application.
+
+## Development
+
+environment building
+
+Install node (^18.12.1) and npm.
+
+Install modules that electron depends on.
+
+ex.) Ubuntu
+
+```
+$ sudo apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libgtk-3-0 libgbm-dev
+```
+
+Launch electron app.
+
+```
+$ npm run dev
+```
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import { BrowserWindow, app } from 'electron';
+
+app.whenReady().then(() => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.resolve(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile('dist/index.html');
+  // mainWindow.webContents.openDevTools({ mode: 'detach' });
+});
+
+app.once('window-all-closed', () => app.quit());
