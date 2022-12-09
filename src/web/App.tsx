26d6136ec080ff9a30f6550615fd38c2be5c6c46
seita1996 <taha.seita@gmail.com>
2022-12-09 19:31:54 +0900

    Update sliders style

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -19,7 +19,7 @@ export const App = () => {
       return <div></div>
     }
     return (
-      <div>
+      <div className='sliderBlock'>
         <Slider min={0} max={23} defaultValue={hour} name={'hour'} setValue={setHour} />
         <Slider min={0} max={59} defaultValue={min} name={'min'} setValue={setMin} />
         <Slider min={0} max={59} defaultValue={sec} name={'sec'} setValue={setSec} />
