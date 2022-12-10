fb9ba39fc02d29dd46eaa397c5fc4e3d10ce7db0
seita1996 <taha.seita@gmail.com>
2022-12-10 13:40:27 +0900

    Hide menu bar

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-import path from 'path';
-import { BrowserWindow, app } from 'electron';
+import path from 'path'
+import { BrowserWindow, app } from 'electron'
 
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
       preload: path.resolve(__dirname, 'preload.js'),
     },
-  });
+  })
 
+  mainWindow.setMenuBarVisibility(false)
   mainWindow.setAlwaysOnTop(true, 'screen-saver')
-  mainWindow.loadFile('dist/index.html');
+  mainWindow.loadFile('dist/index.html')
   // mainWindow.webContents.openDevTools({ mode: 'detach' });
 });
 
-app.once('window-all-closed', () => app.quit());
+app.once('window-all-closed', () => app.quit())
