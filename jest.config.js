b3457d5864fcb6a6e3b0a92498f4863ef1b73212
seita1996 <taha.seita@gmail.com>
2023-01-15 21:14:17 +0900

    Allow to run the test of react's dom

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,8 @@ module.exports = {
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
+  "moduleNameMapper": {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
+  },
 }
--- src/components/__tests__/Clock.spec.tsx
+++ src/components/__tests__/Clock.spec.tsx
@@ -1,5 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react'
+import { Clock } from "../Clock"
+import { render } from "@testing-library/react"
+
 describe("Clock", () => {
-  test("sample", () => {
-    expect(true).toBeTruthy()
+  test("maxSec converted to hh:mm:ss can be obtained", () => {
+    const { getByText } = render(<Clock maxSec={5000} sliderHour={0} silderMin={0} sliderSec={0} setIsTimerProgress={function() {}} />)
+    expect(getByText('01:23:20')).toBeTruthy()
   })
 })
