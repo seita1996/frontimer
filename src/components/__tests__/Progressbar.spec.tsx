557014b7e7e00dfc595ec1d38e35b56ad2fa853e
seita1996 <taha.seita@gmail.com>
2023-01-16 10:33:43 +0900

    Update Progressbar test comment

--- src/components/__tests__/Progressbar.spec.tsx
+++ src/components/__tests__/Progressbar.spec.tsx
@@ -7,7 +7,7 @@ import { Progressbar } from "../Progressbar"
 import { render, screen } from "@testing-library/react"
 
 describe("Progressbar", () => {
-  test("Clicking on reset button will give the value of the slider in hh:mm:ss format", () => {
+  test("Progressbar width to be current / max", () => {
     render(<Progressbar current={5} max={10} />)
     expect(screen.getByTestId('leftbar').style.width).toBe('50%')
   })
