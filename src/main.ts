cf0ddd8c2fa6bb04c29349d97f5f4e4d2c5acca7
seita1996 <taha.seita@gmail.com>
2023-01-18 21:34:00 +0900

    Retains screen size and coordinates when application exits

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import path from 'path'
 import { BrowserWindow, app } from 'electron'
+import Store from 'electron-store'
+
+const store = new Store<StoreType>({
+  /**
+   * Set permissions to -rw-rw-rw- on the file
+   * where you want to save the settings
+   */
+  configFileMode: 0o666,
+  defaults: {
+    x: undefined,
+    y: undefined,
+    width: 800,
+    height: 640,
+  },
+})
 
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
+    x: store.get('x'),
+    y: store.get('y'),
+    width: store.get('width'),
+    height: store.get('height'),
     webPreferences: {
       preload: path.resolve(__dirname, 'preload.js'),
     },
@@ -12,6 +31,11 @@ app.whenReady().then(() => {
   mainWindow.setAlwaysOnTop(true, 'screen-saver')
   mainWindow.loadFile('dist/index.html')
   // mainWindow.webContents.openDevTools({ mode: 'detach' });
-});
+
+  mainWindow.once('close', () => {
+    const { x, y, width, height } = mainWindow.getBounds()
+    store.set({ x, y, width, height })
+  })
+})
 
 app.once('window-all-closed', () => app.quit())
---
+++ src/types/StoreType.d.ts
@@ -0,0 +1,6 @@
+declare type StoreType = {
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
+}
